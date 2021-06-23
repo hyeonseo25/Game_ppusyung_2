ce6bce5fc38fbda9baea66ffde5c17e06acc0b6b
jimin0127 <ckdgh4662@naver.com>
2021-06-24 01:31:11 +0900

    클리어 패널 구현 완료

--- Project/src/panels/Stage1Panel.java
+++ Project/src/panels/Stage1Panel.java
@@ -101,8 +101,9 @@ public class Stage1Panel extends JPanel{
 	private int[] sizeArr; // 이미지의 넓이와 높이를 가져오는 1차원 배열
 	private int[][] colorArr; // 이미지의 x y 좌표의 픽셀 색값을 저장하는 2차원배열
 		
-	private int end = back.getWidth(null)-(view.width-1660); // 도착지
-
+	//private int end = back.getWidth(null)-(view.width-1660); // 도착지
+	private int end = 500;
+	
 	private Player player;
 	private Monster monster;
 	private util.Timer time;
